7a5aa04729efda648874e99402daf65c0fd782cd
brandon025 <brandon025@gmail.com>
2016-10-10 23:09:04 -0700

    Start

---
+++ index.js
@@ -0,0 +1,53 @@
+var monday = [
+
+        {
+
+            'name'     : 'Write a tutorial',
+
+            'duration' : 180
+
+        },
+
+        {
+
+            'name'     : 'Some web development',
+
+            'duration' : 120
+
+        }
+
+    ];
+
+ 
+
+var tuesday = [
+
+        {
+
+            'name'     : 'Keep writing that tutorial',
+
+            'duration' : 240
+
+        },
+
+        {
+
+            'name'     : 'Some more web development',
+
+            'duration' : 180
+
+        },
+
+        {
+
+            'name'     : 'A whole lot of nothing',
+
+            'duration'  : 240
+
+        }
+
+    ];
+
+     
+
+var tasks = [monday, tuesday];
